feaf366ed9c63ad6b94c2f50e65db866f9dde6a7
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-01-22 04:48:59 -0300

    refactor: Alteração na coluna do codigo do produto de varchar 50 para carchar 13.

--- src/main/resources/db/migration/V1__create_table_produto.sql
+++ src/main/resources/db/migration/V1__create_table_produto.sql
@@ -1,6 +1,6 @@
 CREATE TABLE produto(
     id BIGSERIAL PRIMARY KEY,
-    codigo_produto VARCHAR(50) UNIQUE NOT NULL,
+    codigo_produto VARCHAR(13) UNIQUE NOT NULL,
     nome VARCHAR(100) NOT NULL,
     unidade INTEGER NOT NULL,
     validade DATE NOT NULL
